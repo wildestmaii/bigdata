1bbaed39bc3e7247c668f77be214a615a403da5c
Maiara Meneses <52250674+wildestmaii@users.noreply.github.com>
2023-08-31 15:56:52 +0000

    nojeira

--- scraping_beautifulsoap.py
+++ scraping_beautifulsoap.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL da página que queremos fazer scraping
-url = "https://www.bbc.com/news"
+url = "https://jc.ne10.uol.com.br/"
 
 # Conexão: Enviar uma solicitação GET para a URL
 response = requests.get(url)
@@ -14,12 +14,14 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
 
     # Encontre os elementos HTML que contêm os títulos de notícias
-    headlines = soup.find_all("h1", class_="info")
+    caderno = soup.find_all(class_= "hat")
+    headlines = soup.find_all("h1", class_="title")
 
     #cria arquivo csv
     file = open('export_data.csv', 'w', newline='')
     writer = csv.writer(file)
     headers = ['Noticias']
+    hat = [caderno]
     writer.writerow(headers)
 
     # Loop pelos elementos e imprimir os títulos
@@ -27,6 +29,18 @@ if response.status_code == 200:
         print(headline.text)
         #cada noticia
         noticia = [headline.text]
+
+        #salvar noticia no arquivo
+        file = open('export_data.csv', 'a', newline='', encoding='utf-8')
+        writer = csv.writer(file)
+        writer.writerow(noticia)
+        file.close()
+
+    # Loop pelos elementos e imprimir os cadernos
+    for hat in caderno:
+        print(caderno.text)
+        #cada noticia
+        hat = [caderno.text]
 
         #salvar noticia no arquivo
         file = open('export_data.csv', 'a', newline='', encoding='utf-8')
